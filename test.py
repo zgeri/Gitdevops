209ae62c122cf7853b13c21da210156942b4f4e8
zgeri <geri.zeltser@doubleverify.com>
2023-05-14 20:06:42 +0300

    newfile2

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
+<<<<<<< HEAD
 1q
+=======
+print("123")
+print("567")
+print("567")
+print("5sdf67")
+print("5sdfs67")
+print("5ddsfdsf)
+print("567")
+print("567")
+print("56sdf7")
+dsfs
+
+sdfsdsdf
+
+>>>>>>> parent of 0967724...  tejhhjkjstfdsfsd
